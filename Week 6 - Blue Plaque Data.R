a9620d518c9988b398ea1550ac0a6ae1bb155f4e
jacob-echele <ucfnjte@ucl.ac.uk>
2025-11-11 11:57:16 +0000

    3rd commit

--- Week 6 - Blue Plaque Data.R
+++ Week 6 - Blue Plaque Data.R
@@ -8,6 +8,7 @@ library(sf)
 library(tmaptools)
 library(stringr)
 library(dplyr)
+library(tidyverse)
 
 #loading in London Borough Spatial Data
 london_borough <- st_read(here::here("statistical-gis-boundaries-london", "statistical-gis-boundaries-london", "ESRI", "London_Borough_Excluding_MHW.shp"))
@@ -31,3 +32,26 @@ tm_shape(borough_map) +
 tm_shape(blue_plaques) + #fillin with blue plaque data
   tm_dots(fill = "blue", size = 0.1) #symbol shape(color, size)
 
+#getting rid of duplicate data values
+blue_plaques <- distinct(blue_plaques)
+
+#selectung only points within London borough boundaries
+blue_plaques_sub <- blue_plaques[borough_map, ]
+
+#mapping the new blue_plaques_sub data
+tm_shape(borough_map) + 
+  tm_polygons(fill_alpha = 0.5) + #alpha = transparency
+tm_shape(blue_plaques_sub) + #fillin with blue plaque data
+  tm_dots(fill = "blue", size = 0.1) #symbol shape(color, size)
+
+#creates new df with just plaque values that fall within borough_map boundaries
+intersect_indices <-st_intersects(borough_map, blue_plaques) 
+
+#performing Ripley's K analysis on ONE borough
+#this is because it requires a lot of computing power, too many points may tank the analysis
+harrow <- borough_map%>%
+  filter(., NAME == "Harrow")
+
+#displays Harrow borough on map
+tm_shape(harrow) +
+  tm_polygons(col = NA, fill_alpha = 0.5)
