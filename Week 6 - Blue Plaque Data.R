07476f95bcb0c4afe4c743048c96291cd2666f56
jacob-echele <ucfnjte@ucl.ac.uk>
2025-11-14 17:22:45 +0000

    11/14/25 5:22 PM Commit

--- Week 6 - Blue Plaque Data.R
+++ Week 6 - Blue Plaque Data.R
@@ -82,12 +82,65 @@ blue_plaques_harrow_sub_ppp <- ppp(x = blue_plaques_harrow_sub@coords[,1], #mean
 #main = title text
 plot(blue_plaques_harrow_sub_ppp, pch = 16, cex = .5, main = "Blue Plaques - Harrow", cols = "blue") 
 
-#plotting Kernel Density Estimation
+#plotting Kernel Density Estimation ("?density.ppp" for help)
 blue_plaques_harrow_sub_ppp%>%
   density(., sigma = 500)%>%  #'.' means "use all current data", blue_plaques_harrow_sub_ppp;
   #sigma = the spread of the heat map around the point in units as CRS (meters in this case),
   # sigma = 100 (small), sigma = 500 (medium), sigma = 1000 (large)
   plot(., main = "Kernel Density Estimation - Harrow Blue Plaques")
 
+#QUADRAT ANALYSIS
+#plot Quadrat Analysis (counted number of points within grided window) ("?quadratcount.ppp" for help)
+blue_plaques_harrow_sub_ppp%>%
+  quadratcount(., nx = 6, ny = 6)%>% #nx= no. of horizontal squares, ny = no. of vertical squares
+  plot(., col = "red", main = "Quadrat Count - Harrow Blue Plaques") #col colors the count inside each square
+#then plot blue plaque points
+#add = TRUE means you add the next plot (plaque locations) onto the quadrat map, not making a new one
+plot(blue_plaques_harrow_sub_ppp, pch = 16, cex = 0.5, add = TRUE, col = "blue", main = "Blue Plaques - Harrow")
 
+#turning the Quadrat results into a frequency table (df)
+quad_count <- blue_plaques_harrow_sub_ppp%>%
+  quadratcount(., nx = 6, ny = 6)%>%
+  as.data.frame()%>% #creates df
+  dplyr::count(Var1 = Freq)%>% #squares with 0-7 values ("Freq" = frequency)
+  dplyr::rename(Freqquadrantcount = n) #frequency of squares with values of 1-7; n = number of times a square has x points within it
+
+#making sure the data type is numeric (integer)
+quad_count%>%
+  summarize_all(class)
+
+###########################################################
+#   CALCULATING EXPECTED VALUES FOR POISSON DISTRIBUTION
+###########################################################
+sums <- quad_count%>%
+  mutate(total = Var1 * Freqquadrantcount)%>% #adding new column with total (Var1 * Freqquadrantcount)
+  dplyr::summarize(across(everything(), sum))%>% #sums up all the columns in the df
+  dplyr::select(-Var1) #removes Var1 column from the result; leaves with just total no. of squares and total blue plaques
+
+lambda <- quad_count%>% #lambda = mean for Poisson Distribution
+  mutate(total = Var1 * Freqquadrantcount)%>% #adding new column with total
+  dplyr::summarize(across(everything(), sum))%>% #sums all the columns in the df
+  mutate(lambda=total/Freqquadrantcount)%>% #calculating lambda (mean); average no. of plaques per quadrant
+  dplyr::select(lambda)%>% #selecting (keeping) only the lambda column
+  pull(lambda) #making lambda a numeric value, NOT a df
+
+quad_count_table <- quad_count%>% #calculating probability and expected values
+  mutate(probability =((lambda^Var1)*exp(-lambda))/factorial(Var1))%>% #new column for calculating probability; exp = exponent, factorial = x!
+  mutate(Expected=(round(probability*sums$Freqquadrantcount, 0))) #new column fo
+
+####################################################################
+#   PLOTTING OBSERVED VS EXPECTED VALUES FOR POISSON DISTRIBUTION
+####################################################################
+plot(c(1,7), c(0, 14), type = "n", #setting x and y axis ranges
+xlab = "Number of Blue Plaques (Red = Observed, Blue = Expected)",
+  ylab = "Frequency of Occurances",
+  main = "Observed vs. Expected Values - Blue Plaques in Harrow")
+points(quad_count_table$Freqquadrantcount, #selecting observed values
+       col = "red",
+       type = "o", #"overplotted points and lines"; https://www.datacamp.com/doc/r/line for full list for Line Charts in R
+       lwd = 3) #line width
+points(quad_count_table$Expected, #selecting expected values
+       col = "blue",
+       type = "o",
+       lwd = 3)
 
